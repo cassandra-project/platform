c96dc5e254028e019715b33eb4e28bdff404acc0
kvavliak <kvavliak@kvavliakAUTH>
2012-07-19 13:35:24 +0300

    Changed loading the JSON Schemata from relative path to localhost:8080

--- src/eu/cassandra/server/mongo/util/JSONValidator.java
+++ src/eu/cassandra/server/mongo/util/JSONValidator.java
@@ -1,9 +1,15 @@
 package eu.cassandra.server.mongo.util;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
+import java.util.Properties;
 
 import org.codehaus.jackson.map.ObjectMapper;
 
@@ -36,7 +42,7 @@ public class JSONValidator {
 	 * @return
 	 */
 	private String getSchemaFileName(int schemaType) {
-		String fileName = "/home/kvavliak/Dropbox/GitHub/platform3/jsonSchema/";
+		String fileName = "http://localhost:8080/cassandra/resources/jsonSchema/";
 		switch (schemaType) {
 		case ACTIVITY_SCHEMA:  fileName += "Activity.schema";
 		break;
@@ -107,7 +113,7 @@ public class JSONValidator {
 
 		String jsonSchema = readFile(getSchemaFileName(schemaType));
 		if(isUpdate) {
-			
+
 			//			DBObject jsonSchemaObj = (DBObject)JSON.parse(jsonSchema);
 			//			DBObject oidObject = new BasicDBObject("type","string").append("optional", "false").append("properties", new BasicDBObject("$oid",new BasicDBObject("type","string").append("optional", "false")));
 			//			DBObject schemaProperties = (DBObject)jsonSchemaObj.get("properties");
@@ -140,8 +146,9 @@ public class JSONValidator {
 	 * @throws IOException
 	 */
 	private String readFile(String file) throws IOException {
-		FileReader fileReader =  new FileReader (file);
-		BufferedReader reader = new BufferedReader(fileReader);
+		InputStream inStream = new URL(file).openStream();
+		InputStreamReader inputStream = new InputStreamReader(inStream);
+		BufferedReader reader = new BufferedReader(inputStream);
 		String line = null;
 		StringBuilder  stringBuilder = new StringBuilder();
 		String ls = System.getProperty("line.separator");
@@ -150,7 +157,8 @@ public class JSONValidator {
 			stringBuilder.append(ls);
 		}
 		reader.close();
-		fileReader.close();
+		inputStream.close();
+		inStream.close();
 		return stringBuilder.toString();
 	}
 
