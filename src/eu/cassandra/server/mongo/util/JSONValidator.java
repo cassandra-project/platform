845b37d1b28ad15b05cf08e4995310f7f0002b89
kvavliak <kvavliak@kvavliakAUTH>
2012-07-19 13:48:54 +0300

    Change JSON Schema validation library from source to JAR.

--- src/eu/cassandra/server/mongo/util/JSONValidator.java
+++ src/eu/cassandra/server/mongo/util/JSONValidator.java
@@ -1,15 +1,11 @@
 package eu.cassandra.server.mongo.util;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
-import java.util.Properties;
 
 import org.codehaus.jackson.map.ObjectMapper;
 
